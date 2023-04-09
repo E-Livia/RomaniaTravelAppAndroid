675e6a6b61edc8332599df2d90eebdaa32582ae8
E-Livia <livia.popa.3@gmail.com>
2023-04-09 23:54:24 +0300

    update

--- app/src/main/java/com/example/romaniatravelapp/Cities.java
+++ app/src/main/java/com/example/romaniatravelapp/Cities.java
@@ -5,7 +5,7 @@ public class Cities {
     private City[]cities={
             new City(1,
                     "Sighisoara",
-                    "sighisoara1.jpg",
+                    "sighisoara.jpg",
                     "https://en.wikipedia.org/wiki/Sighi%C8%99oara",
                     "Citadel Square, Sighisoara's Clock Tower, The Hill Church",
                     "Sighisoara has a special charm that cannot be described in " +
@@ -13,86 +13,63 @@ public class Cities {
                             "actions are telling stories that are hundreds of years old, " +
                             "marking the evolution of civilization in the Transylvania region."),
             new City(2,
-                    "Sighisoara",
-                    "sighisoara1.jpg",
-                    "https://en.wikipedia.org/wiki/Sighi%C8%99oara",
-                    "Citadel Square, Sighisoara's Clock Tower, The Hill Church",
-                    "Sighisoara has a special charm that cannot be described in " +
-                            "words. It is a place full of history, where the tourist attr" +
-                            "actions are telling stories that are hundreds of years old, " +
-                            "marking the evolution of civilization in the Transylvania region."),
+                    "Brasov",
+                    "brasov.jpg",
+                    "en.wikipedia.org/wiki/Bra%C8%99ov",
+                    "Brasov Historical Center, Black Church, Catherine's Gate",
+                    "Brasov is one of the most beautiful and visited cities in Romania. " +
+                            "It has a rich mix of history, culture, urban fun, and impressive Carpathians’ surroundings. " +
+                            "A top tourist attraction, the city has a great location, " +
+                            "less than three hours by train from Bucharest, Sibiu, and Sighisoara."),
             new City(3,
-                    "Sighisoara",
-                    "sighisoara1.jpg",
-                    "https://en.wikipedia.org/wiki/Sighi%C8%99oara",
-                    "Citadel Square, Sighisoara's Clock Tower, The Hill Church",
-                    "Sighisoara has a special charm that cannot be described in " +
-                            "words. It is a place full of history, where the tourist attr" +
-                            "actions are telling stories that are hundreds of years old, " +
-                            "marking the evolution of civilization in the Transylvania region."),
+                    "Sibiu",
+                    "sibiu.jpg",
+                    "en.wikipedia.org/wiki/Sibiu",
+                    "The Big Square, Astra Museum, Bridge of Lies",
+                    "Sibiu is one of the most charming city in Romania, a place defined by its joie de vivre and soothing atmosphere in summer and fairy-like Christmas Market in winter." +
+                            "Surrounded by wonderful landscapes and a bucolic countryside, the city is a magnet for visitors from all over the world who come to walk on its cobbled streets, admire its colourful buildings and Baroque squares, and discover the traditional architecture in the ASTRA Folk Museum. However, Sibiu is not only defined by what you see today but also by its captivating history. Here are some interesting facts about the city’s old days."),
             new City(4,
-                    "Sighisoara",
-                    "sighisoara1.jpg",
-                    "https://en.wikipedia.org/wiki/Sighi%C8%99oara",
-                    "Citadel Square, Sighisoara's Clock Tower, The Hill Church",
-                    "Sighisoara has a special charm that cannot be described in " +
-                            "words. It is a place full of history, where the tourist attr" +
-                            "actions are telling stories that are hundreds of years old, " +
-                            "marking the evolution of civilization in the Transylvania region."),
+                    "Bucuresti",
+                    "bucuresti.jpg",
+                    "en.wikipedia.org/wiki/Bucharest",
+                    "",
+                    ""),
             new City(5,
-                    "Sighisoara",
-                    "sighisoara1.jpg",
-                    "https://en.wikipedia.org/wiki/Sighi%C8%99oara",
-                    "Citadel Square, Sighisoara's Clock Tower, The Hill Church",
-                    "Sighisoara has a special charm that cannot be described in " +
-                            "words. It is a place full of history, where the tourist attr" +
-                            "actions are telling stories that are hundreds of years old, " +
-                            "marking the evolution of civilization in the Transylvania region."),
+                    "Suceava",
+                    "suceava.jpg",
+                    "en.wikipedia.org/wiki/Suceava",
+                    "",
+                    ""),
             new City(6,
-                    "Sighisoara",
-                    "sighisoara1.jpg",
-                    "https://en.wikipedia.org/wiki/Sighi%C8%99oara",
-                    "Citadel Square, Sighisoara's Clock Tower, The Hill Church",
-                    "Sighisoara has a special charm that cannot be described in " +
-                            "words. It is a place full of history, where the tourist attr" +
-                            "actions are telling stories that are hundreds of years old, " +
-                            "marking the evolution of civilization in the Transylvania region."),
+                    "Cluj-Napoca",
+                    "clujnapoca.jpg",
+                    "en.wikipedia.org/wiki/Cluj-Napoca",
+                    "",
+                    ""),
             new City(7,
-                    "Sighisoara",
-                    "sighisoara1.jpg",
-                    "https://en.wikipedia.org/wiki/Sighi%C8%99oara",
-                    "Citadel Square, Sighisoara's Clock Tower, The Hill Church",
-                    "Sighisoara has a special charm that cannot be described in " +
-                            "words. It is a place full of history, where the tourist attr" +
-                            "actions are telling stories that are hundreds of years old, " +
-                            "marking the evolution of civilization in the Transylvania region."),
+                    "Constanta",
+                    "constanta.jpg",
+                    "en.wikipedia.org/wiki/Constan%C8%9Ba",
+                    "",
+                    ""),
             new City(8,
-                    "Sighisoara",
-                    "sighisoara1.jpg",
-                    "https://en.wikipedia.org/wiki/Sighi%C8%99oara",
-                    "Citadel Square, Sighisoara's Clock Tower, The Hill Church",
-                    "Sighisoara has a special charm that cannot be described in " +
-                            "words. It is a place full of history, where the tourist attr" +
-                            "actions are telling stories that are hundreds of years old, " +
-                            "marking the evolution of civilization in the Transylvania region."),
+                    "Iasi",
+                    "iasi.jpg",
+                    "en.wikipedia.org/wiki/Ia%C8%99i",
+                    "",
+                    ""),
             new City(9,
-                    "Sighisoara",
-                    "sighisoara1.jpg",
-                    "https://en.wikipedia.org/wiki/Sighi%C8%99oara",
-                    "Citadel Square, Sighisoara's Clock Tower, The Hill Church",
-                    "Sighisoara has a special charm that cannot be described in " +
-                            "words. It is a place full of history, where the tourist attr" +
-                            "actions are telling stories that are hundreds of years old, " +
-                            "marking the evolution of civilization in the Transylvania region."),
+                    "Oradea",
+                    "oradea.jpg",
+                    "en.wikipedia.org/wiki/Oradea",
+                    "",
+                    ""),
             new City(10,
-                    "Sighisoara",
-                    "sighisoara1.jpg",
-                    "https://en.wikipedia.org/wiki/Sighi%C8%99oara",
-                    "Citadel Square, Sighisoara's Clock Tower, The Hill Church",
-                    "Sighisoara has a special charm that cannot be described in " +
-                            "words. It is a place full of history, where the tourist attr" +
-                            "actions are telling stories that are hundreds of years old, " +
-                            "marking the evolution of civilization in the Transylvania region.")
+                    "Alba-Iulia",
+                    "alba_iulia.jpg",
+                    "en.wikipedia.org/wiki/Alba_Iulia",
+                    "",
+                    "")
 
     };
 
