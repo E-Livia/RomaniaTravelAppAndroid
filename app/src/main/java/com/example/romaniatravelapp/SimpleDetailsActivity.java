fd33a7f07c2e4abfed49fe05d1d0844339e76724
E-Livia <livia.popa.3@gmail.com>
2023-04-10 01:00:43 +0300

    created simple data activity and simple data layout

--- app/src/main/java/com/example/romaniatravelapp/SimpleDetailsActivity.java
+++ app/src/main/java/com/example/romaniatravelapp/SimpleDetailsActivity.java
@@ -1,4 +1,32 @@
 package com.example.romaniatravelapp;
 
-public class SimpleDetailsActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SimpleDetailsActivity extends AppCompatActivity {
+
+    private TextView cityNameTextView;
+    private TextView attractionsTextView;
+    private TextView descriptionTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_simple_details);
+
+        cityNameTextView=findViewById(R.id.cityName);
+        attractionsTextView=findViewById(R.id.attractionsView);
+        descriptionTextView=findViewById(R.id.descriptionView);
+
+        Intent intent=getIntent();
+        Bundle bundle=intent.getExtras();
+        City cityData=(City)bundle.getSerializable("data");
+
+        cityNameTextView.setText(cityData.getCityName());
+        attractionsTextView.setText(cityData.getAttractions());
+        descriptionTextView.setText(cityData.getDescriptions());
+    }
 }
